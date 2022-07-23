3a241165e6589cf0fc48ae3ff2f8027fc5f87232
Trio Nugroho <ntrio23331@gmail.com>
2022-07-23 14:04:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const agents = ['sova', 'fade', 'breach', 'cyper', 'raze', 'jett'];
+
+const randomAgent = agents[Math.floor(Math.random() * agents.length)];
+
+console.log(`Kamu mendapatkan agent ${randomAgent}`);
