ee6d2a18795d7324131b461487b05d2995998f21
Trio Nugroho <ntrio23331@gmail.com>
2022-07-24 16:08:40 +0700

    random agent

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const agents = ['sova', 'fade', 'breach', 'cyper', 'raze', 'jett'];
+// const agents = ['sova', 'fade', 'breach', 'cyper', 'raze', 'jett'];
+// const randomAgent = agents[Math.floor(Math.random() * agents.length)];
+// console.log(`Kamu mendapatkan agent ${randomAgent}`);
 
-const randomAgent = agents[Math.floor(Math.random() * agents.length)];
+const form = document.querySelector('form');
+const agentSelect = document.querySelector('.agent-select');
 
-console.log(`Kamu mendapatkan agent ${randomAgent}`);
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const values = Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
+        .map(item => item.value)
+    console.log(`Agent yang dipilih ${values}`);
+
+    const randomAgent = values[Math.floor(Math.random() * values.length)];
+
+    agentSelect.innerHTML = randomAgent;
+    
+    console.log(randomAgent);
+});
